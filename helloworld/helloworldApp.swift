9990f302bc3c8336f555423c8f4223a777b119d9
Shafayat Hossain <shafayathossainkhan@gmail.com>
2021-12-28 10:53:58 +0600

    Initial Commit

---
+++ helloworld/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  helloworld
+//
+//  Created by MacBook Pro on 28/12/21.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
